a16cf509d7eae38052f70079249a834bc3871e9a
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2024-04-04 10:03:02 -0400

    Added program

---
+++ index.ts
@@ -0,0 +1,31 @@
+/**
+*
+* This program uses a basic class to make an array of strings
+*
+* By:      Cristiano Sellitto
+* Version: 1.0
+* Since:   2024-03-07
+*/
+
+import { Airplane } from './Airplane'
+import { Jet } from './Jet'
+
+// Create airplane
+const biplane = new Airplane()
+biplane.setSpeed(212)
+console.log(biplane.getSpeed())
+
+// Create jet
+const boeing = new Jet()
+boeing.setSpeed(422)
+console.log(boeing.getSpeed())
+for (let counter = 0; counter < 5; counter++) {
+  boeing.accelerate()
+  console.log(boeing.getSpeed())
+}
+boeing.setSpeed(424)
+boeing.accelerate()
+console.log(boeing.getSpeed())
+
+// Show the program as done
+console.log('\nDone.')
